5508f429b6d9de5f7814a643605981963881dd87
subas <subash.neruturi@gmail.com>
2022-11-05 13:10:10 +0530

    Read Operation Completed for AddressType

---
+++ DataAccessLayer/DataAccess/AddressTypeDataAccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+using DataAccessLayer;
+
+namespace DataAccessLayer.DataAccess
+{
+
+   public class AddressTypeDataAccess
+    {
+        HotelManagement_Context _Context = new HotelManagement_Context();
+
+        public List<AddressTypeModel> GetAllAddressType()
+        {
+            List<tblAddressType> addressTypeList = _Context.tblAddressTypes.ToList();
+            List<AddressTypeModel> addressTypeListBo = new List<AddressTypeModel>();
+
+            foreach (tblAddressType addressType in addressTypeList)
+            {
+                AddressTypeModel addressTypeModel = new AddressTypeModel
+                {
+                    Id = addressType.Id,
+                    Name = addressType.Name,
+                    CREATEDBY = addressType.CREATEDBY,
+                    CREATEDDATE = addressType.CREATEDDATE,
+                    MODIFIEDBY = addressType.MODIFIEDBY,
+                    MODIFIEDDATE = addressType.MODIFIEDDATE,
+                    ISACTIVE = addressType.ISACTIVE
+
+                };
+                addressTypeListBo.Add(addressTypeModel);
+            }
+
+            return addressTypeListBo;
+        }
+
+
+
+    }
+}
---
+++ PresentaionLayer/Controllers/AddressTypeController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogicLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentaionLayer.Controllers
+{
+    public class AddressTypeController : Controller
+    {
+        // GET: AddressType
+      
+        public ActionResult Index()
+        {
+            AddressTypeBusiness atb = new AddressTypeBusiness();
+
+            var addressTypeList = atb.GetAllAddressTypes();
+
+
+            return View(addressTypeList);
+        }
+
+
+
+
+    }
+}
